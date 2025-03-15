16ad06d66a1cde096d9426b2f817f13fa0a90948
Ferdiansyah Mauludin <134785643+dayyysss@users.noreply.github.com>
2025-03-15 08:38:31 +0700

    Update quote_generator.js

--- quote_generator.js
+++ quote_generator.js
@@ -1,14 +1,14 @@
-const fetch = require("node-fetch");
-const fs = require("fs");
+import fetch from "node-fetch";
+import fs from "fs";
 
 async function getQuote() {
-  const res = await fetch("https://api.quotable.io/random");
-  const data = await res.json();
-  const quote = `> "${data.content}" — ${data.author}`;
+    const res = await fetch("https://api.quotable.io/random");
+    const data = await res.json();
+    const quote = `> "${data.content}" — ${data.author}`;
 
-  let readme = fs.readFileSync("README.md", "utf8");
-  readme = readme.replace(/> ".*?" — .*?\n/, quote + "\n"); 
-  fs.writeFileSync("README.md", readme);
+    let readme = fs.readFileSync("README.md", "utf8");
+    readme = readme.replace(/> ".*?" — .*?\n/, quote + "\n");
+    fs.writeFileSync("README.md", readme);
 }
 
 getQuote();
